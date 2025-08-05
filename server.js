f0c793f370b4e5d111e734e0b29e7e511f758028
Mega <syedarbabalishah@gmail.com>
2025-08-05 07:39:41 +0500

    Server fixed

--- server.js
+++ server.js
@@ -11,7 +11,23 @@ const uploadRoutes = require("./routes/uploadRoutes");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+const allowedOrigins = [
+  "https://talesfromthenorthpole.xyz",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
+
 app.use(
   "/uploads",
   (req, res, next) => {
@@ -20,13 +36,13 @@ app.use(
   },
   express.static(path.join(__dirname, "uploads"))
 );
-app.use('/thumbnails', express.static(path.join(__dirname, 'thumbnails')));
+app.use("/thumbnails", express.static(path.join(__dirname, "thumbnails")));
 
 app.use("/auth", authRoutes);
 app.use("/", studentRoutes);
 app.use("/", uploadRoutes);
 app.use("/api/company", companyRoutes);
 
-app.listen(3001, () => {
-  console.log("🚀 Server running at http://localhost:3001");
+app.listen(3100, () => {
+  console.log("🚀 Server running at http://localhost:3100");
 });
